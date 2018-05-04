2bdbdcb56d53be572a1068c5acf3962697e12db9
名賢 邱 <love954009@gmail.com>
2018-05-04 13:30:52 +0800

    練習多型 Polymorphism at work #1 https://docs.microsoft.com/zh-tw/dotnet/csharp/programming-guide/classes-and-structs/polymorphism

---
+++ ConsoleDrawShapes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDrawShapes
+{
+    public class Shape
+    {
+        //A few example members
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        //Virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+    class Circle :Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+            base.Draw();
+        }
+    }
+    class Rectangle :Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle");
+            base.Draw();
+        }
+    }
+    class Triangle :Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Triangle");
+            base.Draw();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Polymorphism at work #1: a Rectangle, Triangle and Circle
+            // can all be used whereever a Shape is expected. No cast is
+            // required because an implicit conversion exists from a derived 
+            // class to its base class.
+            var shapes = new List<Shape>
+        {
+            new Rectangle(),
+            new Triangle(),
+            new Circle()
+        };
+
+            // Polymorphism at work #2: the virtual method Draw is
+            // invoked on each of the derived classes, not the base class.
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
